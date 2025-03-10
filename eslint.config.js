68acf0be1fab36032d8b3c821b39d18250c0ccfd
Mahmoud Fares <mahmoudfaresce@gmail.com>
2025-03-10 06:07:33 +0200

    prettier & lint rules & react compiler

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,54 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import checkFile from 'eslint-plugin-check-file';
+import reactCompiler from 'eslint-plugin-react-compiler';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
+export default {
+   ignores: ['dist'],
+   files: ['**/*.{ts,tsx}'],
+   languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-    },
-    plugins: {
+      parser: tseslint.parser,
+      parserOptions: {
+         projectService: true,
+      },
+   },
+   plugins: {
+      '@typescript-eslint': tseslint.plugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      'check-file': checkFile,
+      'react-compiler': reactCompiler,
+   },
+   rules: {
+      // react-compiler
+      'react-compiler/react-compiler': 'error',
+      // custom rules
+      'prefer-arrow-callback': 'error',
+      'prefer-template': 'error',
+      quotes: ['error', 'single'],
+      'check-file/filename-naming-convention': [
+         'error',
+         {
+            '**/*.{ts,tsx}': 'KEBAB_CASE',
+         },
+         {
+            ignoreMiddleExtensions: true,
+         },
       ],
-    },
-  },
-)
+      'check-file/folder-naming-convention': [
+         'error',
+         {
+            'src/**': 'KEBAB_CASE',
+         },
+      ],
+      'react-refresh/only-export-components': [
+         'warn',
+         { allowConstantExport: true },
+      ],
+      ...reactHooks.configs.recommended.rules,
+   },
+};
